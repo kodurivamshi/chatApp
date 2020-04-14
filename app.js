0de29bf8d1595601376ddd2b01449ab2ee1b366a
kodurivamshi <37183227+kodurivamshi@users.noreply.github.com>
2020-04-15 00:56:30 +0530

    mongo

--- app.js
+++ app.js
@@ -68,11 +68,9 @@ app.use(flash());
 app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
-   next();
+  res.locals.error = req.flash('error');
+ next();
 });
-
-
-
 
 
 // Routes
